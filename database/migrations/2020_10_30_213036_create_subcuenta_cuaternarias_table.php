76dbd8da90b3e4cd1fedec8321049c20ddc04922
Alex Villeda <alxvilleda@live.com>
2020-10-30 21:15:46 -0600

    Script: cambios en estructura de cuentas

--- database/migrations/2020_10_30_213036_create_subcuenta_cuaternarias_table.php
+++ database/migrations/2020_10_30_213036_create_subcuenta_cuaternarias_table.php
@@ -16,7 +16,7 @@ class CreateSubcuentaCuaternariasTable extends Migration
         Schema::create('subcuenta_cuaternarias', function (Blueprint $table) {
             $table->bigIncrements('id_subcuenta_cuaternaria');
             $table->integer('id_subcuenta_terciaria')->unsigned()->foreign()->references('id_subcuenta_terciaria')->on('subcuenta_terciarias')->onDelete('cascade');
-            $table->string('nombre_subcuenta_terciaria',150);
+            $table->string('nombre_subcuenta_cuaternaria',150);
         });
     }
 
--- database/migrations/2020_10_30_213841_create_subcuenta_quinarias_table.php
+++ database/migrations/2020_10_30_213841_create_subcuenta_quinarias_table.php
@@ -16,7 +16,7 @@ class CreateSubcuentaQuinariasTable extends Migration
         Schema::create('subcuenta_quinarias', function (Blueprint $table) {
             $table->bigIncrements('id_subcuenta_quinaria');
             $table->integer('id_subcuenta_cuaternaria')->unsigned()->foreign()->references('id_subcuenta_cuaternaria')->on('subcuenta_cuaternarias')->onDelete('cascade');
-            $table->string('nombre_subcuenta_terciaria',150);
+            $table->string('nombre_subcuenta_quinaria',150);
         });
     }
 
