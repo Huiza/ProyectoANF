924e8a9d3ea7d75c42ec43fe60490e411850e9cf
Alex Villeda <alxvilleda@live.com>
2020-11-18 19:41:22 -0600

    Validacion balance general con JQuery

--- resources/views/EstadosFinancieros/crear_balance_general.blade.php
+++ resources/views/EstadosFinancieros/crear_balance_general.blade.php
@@ -57,7 +57,7 @@
                             
                                 @else
                                 <td>{{$cuenta->cuenta->nombre_cuenta}}</td>
-                                <td><input type="number" step="any" min="0" class=" item form-control round-form" name="saldo[]" placeholder="Monto en $"  required></td>
+                                <td><input onkeyup="calcular_activo()" type="number" step="any" min="0" class=" item form-control round-form monto_activo" name="saldo[]" placeholder="Monto en $"  required></td>
                                 @endif
                                 <input type="text"  name="cuenta[]" value="{{ $cuenta->cuenta->nombre_cuenta }}" hidden>
                                 <div><input type="text"  name="id_estado_financiero[]" value="{{ $estado_financiero->id_estado_financiero}}" hidden></div>
@@ -69,7 +69,7 @@
                                 <td><strong>TOTAL DE ACTIVOS</strong></td>
                                 <div><input type="text"  name="id_estado_financiero[]" value="{{ $estado_financiero->id_estado_financiero}}" hidden></div>
                                 <input type="text"  name="cuenta[]" value="TOTAL DE ACTIVOS" hidden>
-                                <td><input id="total_activos" type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" required></td>
+                                <td><input id="total_activos" type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" readonly></td>
 
                                 
                             @foreach($pasivos as $cuenta)
@@ -80,7 +80,7 @@
                             
                                 @else
                                 <td>{{$cuenta->cuenta->nombre_cuenta}}</td>
-                                <td><input type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" required></td>
+                                <td><input onkeyup="calcular_pasivo()" type="number" step="any" min="0" class="form-control round-form monto_pasivo" name="saldo[]" placeholder="Monto en $" required></td>
                                 @endif
                                 <input type="text"  name="cuenta[]" value="{{ $cuenta->cuenta->nombre_cuenta }}" hidden>
                                 <div><input type="text"  name="id_estado_financiero[]" value="{{ $estado_financiero->id_estado_financiero}}" hidden></div>
@@ -90,7 +90,7 @@
                                 <td><strong>TOTAL DE PASIVOS</strong></td>
                                 <div><input type="text"  name="id_estado_financiero[]" value="{{ $estado_financiero->id_estado_financiero}}" hidden></div>
                                 <input type="text"  name="cuenta[]" value="TOTAL DE PASIVOS" hidden>
-                                <td><input id="total_pasivos" type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" required></td>
+                                <td><input id="total_pasivos" type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" required readonly></td>
 
                                 
                             @foreach($patrimonio as $cuenta)
@@ -101,7 +101,7 @@
                             
                                 @else
                                 <td>{{$cuenta->cuenta->nombre_cuenta}}</td>
-                                <td><input type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" required></td>
+                                <td><input onkeyup="calcular_patrimonio()" type="number" step="any" min="0" class="form-control round-form monto_patrimonio" name="saldo[]" placeholder="Monto en $" required></td>
                                 @endif
                                 <input type="text"  name="cuenta[]" value="{{ $cuenta->cuenta->nombre_cuenta }}" hidden>
                                 <div><input type="text"  name="id_estado_financiero[]" value="{{ $estado_financiero->id_estado_financiero}}" hidden></div>
@@ -111,11 +111,20 @@
                                 <td><strong>TOTAL DE PATRIMONIO</strong></td>
                                 <div><input type="text"  name="id_estado_financiero[]" value="{{ $estado_financiero->id_estado_financiero}}" hidden></div>
                                 <input type="text"  name="cuenta[]" value="TOTAL PATRIMONIO" hidden>
-                                <td><input id="total_patrimonio" type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" required></td>
+                                <td><input id="total_patrimonio" type="number" step="any" min="0" class="form-control round-form" name="saldo[]" placeholder="Monto en $" required readonly></td>
 
                         </table>
-                       
-                       
+                        <div class="col-md-12 alert alert-warning" >
+                          <strong>
+                          <span>TOTAL ACTIVO = </span><span id="validacion_activo"></span>
+                          <br>
+                          <span>TOTAL PASIVO + PATRIMONIO = </span><span id="validacion_pasivo_patrimonio"></span>
+                          </strong>
+                          <br><br>
+                          <p style="color: red" id="mensaje_validacion"></p>
+                        </div>
+
+                        <br><br><br>
                         
                     </div>
                                 
@@ -124,7 +133,7 @@
               </div>
               <div class="form-group">
                     <div class="col-lg-offset-2 col-lg-10">
-                      <button class="btn btn-theme">Guardar</button>
+                      <button class="btn btn-theme" id="boton_guardar" disabled>Guardar</button>
                       <a href="{{route('ver_empresa', $empresa->id)}}" class="btn btn-theme04"> Cancelar</a>
                     </div>
                 </div>
@@ -141,11 +150,68 @@
         </div>
 
         <script>
-          var activos = document.getElementById('total_activos').value;
-          var pasivos = document.getElementById('total_pasivos').value;
-          var patrimonio = document.getElementById('total_patrimonio').value;
-          
-          
+          function calcular_activo() {
+
+          var total = 0;
+          $(".monto_activo").each(function() {
+            if (isNaN(parseFloat($(this).val()))) {
+              total += 0;
+            } else {
+              total += parseFloat($(this).val());
+            }
+          });
+          document.getElementById('total_activos').value = total;
+          document.getElementById('validacion_activo').innerHTML = total;
+          }
+
+
+          function calcular_pasivo() {
+
+          var total = 0;
+          $(".monto_pasivo").each(function() {
+            if (isNaN(parseFloat($(this).val()))) {
+              total += 0;
+            } else {
+              total += parseFloat($(this).val());
+            }
+          });
+          document.getElementById('total_pasivos').value = total;
+          calcular_pasivo_patrimonio();
+          }
+
+
+          function calcular_patrimonio() {
+
+          var total = 0;
+          $(".monto_patrimonio").each(function() {
+            if (isNaN(parseFloat($(this).val()))) {
+              total += 0;
+            } else {
+              total += parseFloat($(this).val());
+            }
+          });
+          document.getElementById('total_patrimonio').value = total;
+
+
+          calcular_pasivo_patrimonio();
+          }
+
+          function calcular_pasivo_patrimonio(){
+            var total_activo = parseFloat(document.getElementById('total_activos').value);
+            var total_pasivo_patrimonio = parseFloat(document.getElementById('total_pasivos').value) + parseFloat(document.getElementById('total_patrimonio').value);
+            document.getElementById('validacion_pasivo_patrimonio').innerHTML = total_pasivo_patrimonio;
+
+            if(total_activo != total_pasivo_patrimonio){
+              document.getElementById('mensaje_validacion').innerHTML = 'EL TOTAL DE ACTIVOS ES DISTINTO DE TOTAL DE PASIVOS + PATRIMONIO. No se puede almacenar.';
+              document.getElementById('boton_guardar').disabled = true;
+            }
+            else{
+              document.getElementById('mensaje_validacion').innerHTML = '';
+              document.getElementById('boton_guardar').disabled = false;
+            }
+          }
+
+
         </script>
 
 @endsection
